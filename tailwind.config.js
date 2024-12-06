95ddcb82dd240f32f8472dd8a1a98eefddf78ea2
AbsorbedPoem <alejandrovgz0804@gmail.com>
2024-12-05 20:11:05 -0400

    layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-    content: ["./*.{html}"],
+    content: ["./templates/*.{html,js}"],
+    darkMode: 'slector',
     theme: {
-        extend: {},
+        fontFamily: {
+            sans: ["Inter var", 'sans-serif'],
+            serif: ['Merriweather', 'serif'],
+        },
+        colors: {
+            transparent: 'transparent',
+            'main': colors.blue,
+            black: colors.black,
+            white: colors.white,
+            slate: colors.slate,
+            gray: colors.gray,
+            zinc: colors.zinc,
+            neutral: colors.neutral,
+            stone: colors.stone,
+            red: colors.red,
+            orange: colors.orange,
+            amber: colors.amber,
+            yellow: colors.yellow,
+            lime: colors.lime,
+            green: colors.green,
+            emerald: colors.emerald,
+            teal: colors.teal,
+            cyan: colors.cyan,
+            sky: colors.sky,
+            blue: colors.blue,
+            indigo: colors.indigo,
+            violet: colors.violet,
+            purple: colors.purple,
+            fuchsia: colors.fuchsia,
+            pink: colors.pink,
+            rose: colors.rose
+        }
     },
     plugins: [],
 }
